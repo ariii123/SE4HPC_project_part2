25f37e80ada8541edd8eed49956d2011d30adc53
ari <arianna.procaccio@mail.polimi.it>
2024-06-03 13:53:11 +0000

    job

--- job.sh
+++ job.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#SBATCH --job-name=matMult
+#SBATCH --output=matMult.out
+#SBATCH --time=00:10 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=10M
 
 singularity exec singularity/cont.sif mpirun -n 2 /home/repo/build/main ./matrixA.txt ./matrixB.txt > output/stdout.txt 2> output/stderr.txt
