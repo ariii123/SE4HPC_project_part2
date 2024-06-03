544c41289bf2f026c1316eecb4c954ca94417908
Denis Untila <denis.untila@mail.polimi.it>
2024-06-03 16:27:39 +0000

    Testing the whole project

--- job.sh
+++ job.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=matMult
-#SBATCH --output=matMult.out
+#SBATCH --output=/dev/null
 #SBATCH --time=00:10 
-#SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=10M
+#SBATCH --ntasks=2
 
-singularity exec cont.sif mpirun -n 2 /home/repo/build/main ./matrixA.txt ./matrixB.txt > stdout.txt 2> stderr.txt
+
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+singularity exec --bind /scratch_local:$TMPDIR cont.sif mpirun -n 2 /home/repo/build/main ./matrixA.txt ./matrixB.txt > stdout.txt 2> stderr.txt
+
+rm -rf $TMPDIR
