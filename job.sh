89110f2c966ff0cd90c3cf5533786440e6b7bf3f
Denis Untila <denis.untila@mail.polimi.it>
2024-06-03 15:33:33 +0000

    trying to remotly run the container via ssh

--- job.sh
+++ job.sh
@@ -7,4 +7,4 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=10M
 
-singularity exec singularity/cont.sif mpirun -n 2 /home/repo/build/main ./matrixA.txt ./matrixB.txt > output/stdout.txt 2> output/stderr.txt
+singularity exec cont.sif mpirun -n 2 /home/repo/build/main ./matrixA.txt ./matrixB.txt > stdout.txt 2> stderr.txt
